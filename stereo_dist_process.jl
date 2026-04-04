e6d869b57b83a0128b0b73b8169e1e0ce46104a7
Shiva Darshan <shiva.darshan@essec.edu>
2026-04-04 06:30:12 +0200

    recoded to use new formulation of distance process

--- stereo_dist_process.jl
+++ stereo_dist_process.jl
@@ -6,25 +6,23 @@ using Random
 
 norm_dist::UnivariateDistribution = Normal()
 
-function transformed_mr_dist_p_inc(y, h, χ_dist, B_dist, RNG)
+function mr_dist_inc(y, h, d, RNG)
 	U = rand(RNG)
 	if U <= 2*ccdf(norm_dist, sqrt(y/(h^2*(2-y))))
 		return 0
 	else
-		X, B = rand(RNG, χ_dist), rand(RNG, B_dist)
+		υ, ξ = rand(RNG, Chisq(1)), rand(RNG, Chisq(d-1))
 
-		return (1 - h^2*(1 + B)*X/(1 + h^2*X))*y + 2*B*h^2*X/(1 + h^2*X)
+		return (y + h^2 * υ*(2 -y))/(1 + h^2 * (υ + ξ))
 	end
 end
 
-function transformed_mr_dist_traj!(traj, y_0, h, d, RNG)
-	χ_dist = Chi(d/2)
-	B_dist = Beta(1/2, d/2)
+function mr_dist_traj!(traj, y_0, h, d, RNG)
 	y = copy(y_0)
 	traj[1] = y
 	for i in axes(traj, 1)
 		if i >= 2
-			y = transformed_mr_dist_p_inc(y, h, χ_dist, B_dist, RNG)
+			y = mr_dist_inc(y, h, d, RNG)
 			traj[i] = y
 		end
 	end
@@ -32,50 +30,21 @@ function transformed_mr_dist_traj!(traj, y_0, h, d, RNG)
 	return traj
 end
 
-function transformed_reflect_dist_p_inc(y, h, χ_dist, B_dist, RNG)
-	X, B = rand(RNG, χ_dist), rand(RNG, B_dist)
+function sim_meeting_times(N, y_0, h, d)
+	τs = Array{Float64, 1}(undef, N)
 
-	return (1 - h^2*(1 + B)*X/(1 + h^2*X))*y + 2*B*h^2*X/(1 + h^2*X)
-end
-
-function transformed_reflect_dist_traj!(traj, y_0, h, d, RNG)
-	χ_dist = Chi(d/2)
-	B_dist = Beta(1/2, d/2)
-	y = copy(y_0)
-	traj[1] = y
-	for i in axes(traj, 1)
-		if i >= 2
-			y = transformed_reflect_dist_p_inc(y, h, χ_dist, B_dist, RNG)
-			traj[i] = y
+	Threads.@threads for i in axes(τs, 1)
+		Y = copy(y_0)
+		local RNG = Xoshiro(i)
+		local k = 0
+		while Y > 0
+			Y = mr_dist_inc(Y, h, d, RNG)
+			k += 1
 		end
+		τs[i] = k
 	end
 
-	return traj
+	return τs
 end
 
 
-function simulate_mr_dist_trajs(initial_dist, h, d, time_horizon, N, rng_seed = 0)
-	trajs = zeros(time_horizon, N)
-	y_0 = 1 - cos(initial_dist)
-	Threads.@threads for j in axes(trajs, 2)
-		RNG = Xoshiro(rng_seed + j)
-		#simulate 1 - cos(dist_k) process. i.e. the transformed dist process
-		transformed_mr_dist_traj!(view(trajs, :, j), y_0, h, d, RNG)
-	end
-
-	#transform back and return the distance trajectory
-	return acos.(1. .- trajs)
-end
-
-function simulate_reflect_dist_trajs(initial_dist, h, d, time_horizon, N, rng_seed = 0)
-	trajs = zeros(time_horizon, N)
-	y_0 = 1 - cos(initial_dist)
-	Threads.@threads for j in axes(trajs, 2)
-		RNG = Xoshiro(rng_seed + j)
-		#simulate 1 - cos(dist_k) process. i.e. the transformed dist process
-		transformed_reflect_dist_traj!(view(trajs, :, j), y_0, h, d, RNG)
-	end
-
-	#transform back and return the distance trajectory
-	return acos.(1. .- trajs)
-end
