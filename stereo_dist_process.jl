cbb602b56a69a7a4bb6ff13525c0709e3414704c
Shiva Darshan <shiva.darshan@essec.edu>
2026-04-05 09:12:58 +0200

    reorganization

--- stereo_dist_process.jl
+++ stereo_dist_process.jl
@@ -4,41 +4,124 @@ using LoopVectorization
 using Distributions
 using Random
 
-norm_dist::UnivariateDistribution = Normal()
+include("samplers.jl")
 
-function mr_dist_inc(y, h, d, RNG)
-	U = rand(RNG)
-	if U <= 2*ccdf(norm_dist, sqrt(y/(h^2*(2-y))))
-		return 0
-	else
-		υ, ξ = rand(RNG, Chisq(1)), rand(RNG, Chisq(d-1))
 
-		return (y + h^2 * υ*(2 -y))/(1 + h^2 * (υ + ξ))
-	end
-end
 
-function mr_dist_traj!(traj, y_0, h, d, RNG)
+function reflect_traj(x_0, y_0, h, K, RNG)
+
+	x_traj = zeros(length(x_0), K)
+	y_traj = zeros(length(y_0), K)
+
+	υ_traj = zeros(K)
+	ξ_traj = zeros(K)
+
+	x = copy(x_0)
+	X_new = copy(x_0)
 	y = copy(y_0)
-	traj[1] = y
-	for i in axes(traj, 1)
-		if i >= 2
-			y = mr_dist_inc(y, h, d, RNG)
-			traj[i] = y
-		end
+	G = randn(length(x_0))
+	G̃ = randn(length(x_0))
+
+	d = length(x_0) - 1 #dimension of the sphere
+
+	for i in axes(x_traj, 2)
+		randn!(RNG, G)
+		υ_traj[i] = dot(get_e_x_perp_y(x,y), G)^2
+		ξ_traj[i] = norm(G .- dot(x, G) .* x .- dot(get_e_x_perp_y(x,y), G) .* get_e_x_perp_y(x,y))^2
+
+		reflection_coupling_step!(x, y, G, G̃, h, RNG)
+		x_traj[:, i] .= x
+		y_traj[:, i] .= y
 	end
 
-	return traj
+	return x_traj, y_traj, υ_traj, ξ_traj
 end
 
-function sim_meeting_times(N, y_0, h, d)
+function parallel_traj_test(x_0, y_0, h, K, RNG)
+
+	x_traj = zeros(length(x_0), K)
+	y_traj = zeros(length(y_0), K)
+
+	υ_traj = zeros(K)
+	ξ_traj = zeros(K)
+
+	x = copy(x_0)
+	X_new = copy(x_0)
+	y = copy(y_0)
+	G = randn(length(x_0))
+	G̃ = randn(length(x_0))
+
+	d = length(x_0) - 1 #dimension of the sphere
+
+	for i in axes(x_traj, 2)
+		randn!(RNG, G)
+		υ_traj[i] = dot(get_e_x_perp_y(x,y), G)^2
+		ξ_traj[i] = norm(G .- dot(x, G) .* x .- dot(get_e_x_perp_y(x,y), G) .* get_e_x_perp_y(x,y))^2
+
+		parallel_coupling_step!(x, y, G, G̃, h, RNG)
+		x_traj[:, i] .= x
+		y_traj[:, i] .= y
+	end
+
+	return x_traj, y_traj, υ_traj, ξ_traj
+end
+
+function reflect_dist_traj(x_0, y_0, h, υ, ξ)
+
+
+	dist_traj = zeros(length(υ))
+	Y = 1. .- dot(x_0, y_0)
+
+	for i in axes(υ, 1)
+		Y = ((1. - h^2 * υ[i])*Y + 2. * h^2)/(1 + h^2 * (υ[i] + ξ[i]))
+		dist_traj[i] = Y
+	end
+
+	return dist_traj
+end
+
+function mr_traj_test(x_0, y_0, h, K, RNG)
+	
+	x_traj = zeros(length(x_0), K)
+	y_traj = zeros(length(y_0), K)
+
+	υ_traj = zeros(K)
+	ξ_traj = zeros(K)
+
+	x = copy(x_0)
+	X_new = copy(x_0)
+	y = copy(y_0)
+	G = randn(length(x_0))
+	G̃ = randn(length(x_0))
+
+	d = length(x_0) - 1 #dimension of the sphere
+
+	for i in axes(x_traj, 2)
+		randn!(RNG, G)
+		υ_traj[i] = dot((y .- dot(x,y).*x), G)/sqrt(1 - dot(x,y)^2)
+		ξ_traj[i] = norm(G .- dot(G, x) .* x .- (dot((y .- dot(x,y).*x), G)/(1 - dot(x,y)^2)) .* (y .- dot(x,y).*x))^2
+
+		max_reflection_coupling_step!(x, y, X_new, G, G̃, h, d, RNG)
+		x_traj[:, i] .= x
+		y_traj[:, i] .= y
+	end
+
+	return x_traj, y_traj, υ_traj, ξ_traj
+end
+
+function sim_meeting_times(N, h, d)
 	τs = Array{Float64, 1}(undef, N)
 
-	Threads.@threads for i in axes(τs, 1)
-		Y = copy(y_0)
-		local RNG = Xoshiro(i)
-		local k = 0
-		while Y > 0
-			Y = mr_dist_inc(Y, h, d, RNG)
+	Threads.@threads for i in tqdm(axes(τs, 1))
+		RNG = Xoshiro(i)
+		x = normalize(randn(RNG, d))
+		X_new = copy(x)
+		y = -copy(x)
+		G = randn(RNG, d)
+		G̃ = randn(RNG, d)
+		k = 0
+		while x != y
+			max_reflection_coupling_step!(x, y, X_new, G, G̃, h, d, RNG)
 			k += 1
 		end
 		τs[i] = k
